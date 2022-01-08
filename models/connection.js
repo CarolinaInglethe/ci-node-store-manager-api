d1a6177ce0d97bdac52d043ae7c5d4f47f4d0c62
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-08 00:04:21 -0300

    refatora connection

--- models/connection.js
+++ models/connection.js
@@ -3,8 +3,8 @@ const { promise } = require('sinon');
 require('dotenv').config();
 
 const OPTIONS = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
 const MONGO_DB_URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/StoreManager`;
@@ -12,12 +12,15 @@ const DB_NAME = 'StoreManager';
 
 let db = null;
 
-const connection = () => (db
+const connection = () => (
+    db
     ? promise.resolve(db)
     : MongoClient.connect(MONGO_DB_URL, OPTIONS)
     .then((conn) => { 
       db = conn.db(DB_NAME);
       return db; 
-    }));
+    })
+    .catch((err) => console.log(err.message))
+  );
 
 module.exports = connection;
