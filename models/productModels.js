64d34fe35b06719dcc0b1b56fa202c036cd156ed
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-07 22:55:39 -0300

    coments

--- models/productModels.js
+++ models/productModels.js
@@ -1,14 +1,13 @@
 const connection = require('./connection');
 
-const create = async (name, quantity) => {
-    await connection()
+// Requesito 1 :
+const create = async (name, quantity) => connection()
     .then((db) => db.collection('products').insertOne({ name, quantity }))
     .then((result) => ({
         id: result.insertedId,
         name,
         quantity,
         }));
-};
 
 module.exports = {
     create,
