d3cf4d93abe594506830ded32ca56fa657738b08
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-08 14:41:19 -0300

    Req 1 (create) - refatora Camada Models conexao

--- models/connection.js
+++ models/connection.js
@@ -1,26 +1,21 @@
 const { MongoClient } = require('mongodb');
-const { promise } = require('sinon');
 require('dotenv').config();
+
+const MONGO_DB_URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/StoreManager`;
+const DB_NAME = 'StoreManager';
 
 const OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/StoreManager`;
-const DB_NAME = 'StoreManager';
-
-let db = null;
-
 const connection = () => (
-    db
-    ? promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
-    .then((conn) => { 
-      db = conn.db(DB_NAME);
-      return db; 
+   MongoClient.connect(MONGO_DB_URL, OPTIONS)
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => {
+      console.log(err);
+      process.exit(1);
     })
-    .catch((err) => console.log(err.message))
   );
 
 module.exports = connection;
