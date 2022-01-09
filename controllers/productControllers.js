a780337f39998957e87d4ae311a274a69d79502b
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-09 14:03:16 -0300

    Req 2 (getAll/getById)- Camada Controllers, implementa funçoes e chamada p/ proxima camada

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -14,16 +14,27 @@ const create = async (req, res, _next) => {
 };
 
 // Requesito 2 :
-const getAll = async (_req, res, _next) => res.status(200)
-.json({ message: 'retorna todos produtos' });
+const getAll = async (_req, res, _next) => {
+    const AllProducts = await productServices.getAll();
+
+    if (AllProducts.err) {
+        return res.status(422).json(AllProducts);
+    }
+
+    return res.status(200).json(AllProducts);
+};
 
 const getById = async (req, res, _next) => {
     const { id } = req.params;
-    if (Number(id)) {
-      return res.status(200)
-        .json({ message: 'retorna produto de determinado id' }); 
+    const productById = await productServices.getById(id);
+
+    if (productById.err) {
+        return res.status(422).json(productById);
     }
+
+    return res.status(200).json(productById);
 };
+
 module.exports = {
     create,
     getAll,
