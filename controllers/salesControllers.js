bf3b754f4f0c98f045feb59d3b95ef13f4eb1311
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-10 19:48:42 -0300

    refatora salesControllers

--- controllers/salesControllers.js
+++ controllers/salesControllers.js
@@ -2,14 +2,13 @@ const salesServices = require('../services/salesSevices');
 
 const create = async (req, res) => {
     const { productId, quantity } = req.body;
-
     const createdSales = await salesServices.create(productId, quantity);
-
+    
     if (createdSales.err) {
         return res.status(422).json(createdSales);
     }
 
-    res.status(200).json(createdSales);
+    return res.status(200).json(createdSales);
 };
 
 module.exports = {
