b094e2fdc6a897633ddfe9494d0860c101c18a80
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-10 20:37:30 -0300

    Req 6(getSales) -Camada Models , implementa querys e conexoes

--- models/salesModels.js
+++ models/salesModels.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb');
 const mongoConnection = require('./connection');
 
 // Requesito 5 :
@@ -10,6 +11,25 @@ const create = async (productId, quantity) => {
     return createdSales;
   };
 
+// Requesito 6:
+const getAll = async () => {
+  const connection = await mongoConnection();
+  const allSales = await connection.collection('sales').find().toArray();
+
+  return allSales;
+};
+
+const getById = async (id) => {
+  if (!ObjectId.isValid(id)) return false;
+  const connection = await mongoConnection();
+  const productById = await connection.collection('sales')
+    .findOne({ _id: ObjectId(id) });
+
+  return productById;
+};
+
 module.exports = {
     create,
+    getAll,
+    getById,
 };
