40746bd6dda1c55665ae5cf73c159e6285a9252a
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-10 19:12:30 -0300

    Req 5(createSales) - implementa Camada Controllers -resposta de rotas

---
+++ controllers/salesControllers.js
@@ -0,0 +1,15 @@
+const create = async (req, res) => {
+    const { productId, quantity } = req.body;
+
+    const createdSales = await salesServices.create(productId, quantity);
+
+    if (createdSales.err) {
+        return res.status(422).json(createdSales);
+    }
+
+    res.status(201).json(createdSales);
+};
+
+module.exports = {
+    create,
+};
