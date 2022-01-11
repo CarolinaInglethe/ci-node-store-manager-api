36c3db712338ed2b6e81753bb777ebf4c37041b1
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-11 13:11:46 -0300

    refatora salesValidations

--- services/utils/salesValidation.js
+++ services/utils/salesValidation.js
@@ -1,17 +1,15 @@
 const validateQuantity = (arraySales) => {
-  const err = {
-    err: {
-      code: 'invalid_data',
-      message: 'Wrong product ID or invalid quantity',
-    },
-  };
-
   // passa por cada sale e valida seu quantity
   for (let i = 0; i < arraySales.length; i + 1) {
- if (arraySales[i].quantity <= 0 || typeof arraySales[i].quantity === 'string') {
-      return err;
+    if (arraySales[i].quantity <= 0 || typeof arraySales[i].quantity === 'string') {
+      return {
+        err: {
+          code: 'invalid_data',
+          message: 'Wrong product ID or invalid quantity',
+        },
+      };
     } 
-}
+  }
 };
 
 module.exports = {
