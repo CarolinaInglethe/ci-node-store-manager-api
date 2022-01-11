eef537a89d57c48f7039f35cc339a6ec22ad7b96
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-10 23:22:46 -0300

    Refatora Req 5 - Camada Models , Trabalha com inserçao com chave extra e seu retorno completo

--- models/salesModels.js
+++ models/salesModels.js
@@ -2,13 +2,13 @@ const { ObjectId } = require('mongodb');
 const mongoConnection = require('./connection');
 
 // Requesito 5 :
-const create = async (productId, quantity) => {
+const create = async (arraySales) => {
     const connection = await mongoConnection();
 
-    const createdSales = await connection.collection('sales')
-    .insertOne({ productId, quantity });
+    const { insertedId: id } = await connection.collection('sales')
+    .insertOne({ itensSold: arraySales });
   
-    return createdSales;
+    return { _id: id, itensSold: arraySales };
   };
 
 // Requesito 6:
